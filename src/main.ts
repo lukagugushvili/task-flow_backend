4e9fefb758a5efeb7532ebf2b0fbe1231da31ee2
lukagugushvili <gugushvili58@gmail.com>
2024-11-13 02:23:48 -0800

    Enable CORS configuration using ConfigService

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as morgan from 'morgan';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(morgan('tiny'));
-  await app.listen(4000);
+
+  const configService = app.get(ConfigService);
+  const corsOrigin = configService.get<string>('CORS_Origin');
+
+  app.enableCors({
+    origin: corsOrigin,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(process.env.Port ?? 3000);
 }
 bootstrap();
